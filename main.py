c97212cb4328daf19e5c5725dc32702308286e97
mikha-el82 <mikhael@centrum.cz>
2022-02-03 15:27:43 +0100

    Final - with mail sending commented out

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-##################### Extra Hard Starting Project ######################
+# Extra Hard Starting Project #
 
 # 1. Update the birthdays.csv
 
 # 2. Check if today matches a birthday in the birthdays.csv
 
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual
+# name from birthdays.csv
 
 # 4. Send the letter generated in step 3 to that person's email address.
 
@@ -12,7 +13,7 @@ import datetime as dt
 import random
 import pandas as pd
 import os
-import smtplib
+# import smtplib
 
 data = pd.read_csv("birthdays.csv")
 
